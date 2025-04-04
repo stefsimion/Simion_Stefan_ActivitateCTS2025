1ab0ac0c966d9f34ae73dc1be6523d2ffd0263a1
Stefan Simion <stefansimion@effluence.space>
2025-04-04 16:07:00 +0300

    Seminar6

--- Seminar6/src/ro/cts/main/module/AdaptorMedicament.java
+++ Seminar6/src/ro/cts/main/module/AdaptorMedicament.java
@@ -1,4 +1,20 @@
+
 package ro.cts.main.module;
 
-public class AdaptorMedicament {
+public class AdaptorMedicament extends MedicamentFarmacie{
+    private MedicamentSpital medicamentSpital;
+
+    public AdaptorMedicament(String nume, int pret, boolean stoc, String dataExpirare) {
+        super(nume, pret, stoc, dataExpirare);
+    }
+
+    public AdaptorMedicament(MedicamentSpital medicamentSpital) {
+        super(medicamentSpital.getNume(), medicamentSpital.getPret(), true, "12.04.2027");
+        this.medicamentSpital = medicamentSpital;
+    }
+
+    @Override
+    public void cumparaMedicament() {
+        medicamentSpital.achizitioneazaMedicament();
+    }
 }
--- Seminar6/src/ro/cts/main/module/MedicamentSpital.java
+++ Seminar6/src/ro/cts/main/module/MedicamentSpital.java
@@ -1,4 +1,29 @@
+
 package ro.cts.main.module;
 
 public class MedicamentSpital {
+    private String nume;
+    private int pret;
+
+    public MedicamentSpital(String nume, int pret) {
+        this.nume = nume;
+        this.pret = pret;
+    }
+
+    public String getNume() {
+        return nume;
+    }
+
+    public int getPret() {
+        return pret;
+    }
+
+    public void achizitioneazaMedicament(){
+        prezintaReteta();
+        System.out.println("Medicamentul "+nume+" costa "+pret);
+    }
+
+    public void prezintaReteta(){
+        System.out.println("A fost aratata reteta");
+    }
 }
